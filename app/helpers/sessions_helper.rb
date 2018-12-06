d3f814a7f7b008a9c7b72b289ed6c922f9c8aa12
Vicky <vii.zhu@gmail.com>
2018-12-05 21:48:12 -0500

    user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if(user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -25,15 +29,26 @@ module SessionsHelper
     !current_user.nil?
   end
   
-def forget(user)
-  user.forget
-  cookies.delete(:user_id)
-  cookies.delete(:remember_token)
-end
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default location)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
